d27cf6fbf734832329b2709750c9414af4d2c3f7
Akihiro1001 <takiaki1001e@gmail.com>
2022-10-10 15:54:06 +0900

    pprof.sh修正

--- pprof.sh
+++ pprof.sh
@@ -3,5 +3,8 @@
 # 分析データのリセット
 rm -rf /home/isucon/pprof/
 
-# 分析データの収集
-go tool pprof -http=0.0.0.0:8080 /home/isucon/webapp/go/isucondition http://localhost:6060/debug/pprof/profile
+# 公開サーバーのプロセス停止
+kill -9 $(lsof -t -i :8080)
+
+# 公開サーバー起動、分析データの収集
+nohup go tool pprof -http=0.0.0.0:8080 /home/isucon/webapp/go/isucondition http://localhost:6060/debug/pprof/profile &
