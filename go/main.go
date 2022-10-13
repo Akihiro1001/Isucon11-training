fe36676aed295ef25d98bc6d27e4fe2a2bd12b39
akihiro <takiaki1001e@gmail.com>
2022-10-13 09:06:11 +0900

    アプリ側のログ出力が多すぎる問題を改善

--- go/main.go
+++ go/main.go
@@ -214,10 +214,10 @@ func main() {
 		log.Fatal(http.ListenAndServe(":6060", nil))
 	}()
 	e := echo.New()
-	e.Debug = true
-	e.Logger.SetLevel(log.ERROR)
+	// e.Debug = true
+	// e.Logger.SetLevel(log.ERROR)
 
-	e.Use(middleware.Logger())
+	// e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	e.POST("/initialize", postInitialize)
@@ -1242,7 +1242,7 @@ func postIsuCondition(c echo.Context) error {
 	// TODO: 一定割合リクエストを落としてしのぐようにしたが、本来は全量さばけるようにすべき
 	dropProbability := 0.9
 	if rand.Float64() <= dropProbability {
-		c.Logger().Warnf("drop post isu condition request")
+		// c.Logger().Warnf("drop post isu condition request")
 		return c.NoContent(http.StatusAccepted)
 	}
 
